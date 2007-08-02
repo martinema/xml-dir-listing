54a3ae282f958df619397e1319eeab0a688a25e5
monsieur.haynes <monsieur.haynes@f29ea386-3431-0410-afd1-c10b351bfbf8>
2007-08-02 17:30:51 +0000

    Done so far

--- src/net/matthaynes/xml/dirlist/FileListing.java
+++ src/net/matthaynes/xml/dirlist/FileListing.java
@@ -1,195 +1,283 @@
-package net.matthaynes.xml.dirlist;
+/* 
+ * TODO:
+ * 
+ * Error checking everywhere!
+ * Logging.
+ * Sorting
+ * Reverse sorting
+ * Depth
+ * Includes 
+ * Excludes
+ * Seperate Main into another class
+ * Ant Task
+ * 
+ */
 
-import java.util.*;
+// package net.matthaynes.xml.dirlist;
+
 import java.io.*;
+import java.util.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
-/**
-* Directory Listing to XML.
-* Matt Haynes, October 2006
-*
-* Recursively searches a given directory and all subdirectories and produces an XML representation.
-*
-* Example Usage:
-*    java FileListing -v -h "C:\Program Files\Test Directory" "output.xml"
-*
-* To do:
-*  - Get code reviewed
-*  - Move write output file functionality to it's own function
-*  - Do all checks before any processing, ie. is given output file valid?
-*  - Allow the directory argument to take an ending /
-*  - Possibly improve on sort functionality, so all directories appear at top, then files, all alphabetical.
-*  - Tidy, better comments, optimize
-*  - Improve checks for correct arguments.
-*  - Improve error messages
-*  - Improve verbose info
-*  - Version info
-*  - A filter so only certain files or folders are indexed. Could be regEx with eval().
-**/
+//SAX classes.
+import org.xml.sax.*;
+import org.xml.sax.helpers.*;
+
+//JAXP 1.1
+import javax.xml.parsers.*;
+import javax.xml.transform.*;
+import javax.xml.transform.stream.*;
+import javax.xml.transform.sax.*;
 
 public final class FileListing {
-
-	// Global variables
-	public static String output = "";
-	public static String outputFile = "";
-	public static Boolean verbose = false;
-
-
+	
+	public static StreamResult streamResult;
+	public static SAXTransformerFactory tf;
+	public static AttributesImpl atts;
+	public static TransformerHandler hd;
+	public static Transformer serializer;
+	
+	public static DateFormat dateFormat = new SimpleDateFormat("yyyyMMdd'T'HHmmss");
+	
 	// Main function
-	public static void main(String... aArguments) throws FileNotFoundException {
-
-			// Get output file
-			outputFile = aArguments[aArguments.length - 1];
-
-			// Get Arguments and flags
-			for(int i=0; i < aArguments.length; i++) {
-
-				String thisArg = aArguments[i];
-
-				if (thisArg.equals("-v") || thisArg.equals("-verbose") ) {
-					// Verbose
-					verbose = true;
-
-				} else if (thisArg.equals("-help") || thisArg.equals("-h") || thisArg.equals("-?")) {
-
-					// Run Help function
-					generateHelpInformation();
-
-				} else if (i ==  aArguments.length - 2) {
-
-			     	// Call generation function, pass it directory name
-					generateXmlListing(thisArg);
-
-				}
-			}
+	public static void main(String aArguments[]) throws FileNotFoundException, IOException {
+	
+		// Get specified directory
+		File dir = new File(aArguments[1]); 
+		
+		// Set output stream for generated file
+		FileOutputStream out = new FileOutputStream("test.xml");
+		
+		// Begin listing
+		generateXmlListing(dir, out);
+		
+		// Close output stream
+		out.close();
 	}
-
-
-  public static void generateXmlListing(String directory) {
-
-		// Get directory
-	 	File dir =  new File(directory);
-
-	  	// Check dir is a directory
+	
+	/**
+	 * Starts generation of XML directory listing.
+	 * @param dir The directory to begin listing.
+	 */
+	public static void generateXmlListing(File dir, FileOutputStream out) {
+		
+				
 		if (dir.isDirectory()) {
-
-			// Tell the command whats going on, because it can take a while!
-
-			System.out.println("\nAnalysing directory...");
-
-			output += "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
-			output += "<fileList>";
-
-			// Call main function
-			visitAllDirsAndFiles(dir);
-
-			output += "</fileList>";
-
-			// Get the directory the output file is in
-			String outputParent = new File(outputFile).getParent();
-
-			// Check it exists, if not create it
-			if (outputParent != null && new File(outputParent).isDirectory() == false) {
-
-				// Output information on command line if verbose is true
-				if (verbose == true) {
-					System.out.println("\nAttempting to generate the directory "+ outputParent);
-				}
-
-				// Make directories
-				new File(outputParent).mkdirs();
-			}
-
-			// Output information on command line if verbose is true
-			if (verbose == true) {
-				System.out.println("\nAttempting to write to "+ outputFile);
-			}
-
-			// Write directory listing to xml file
+			
+			System.out.print("Generating listing for " + dir.getAbsolutePath());
+			
 			try {
-				BufferedWriter out = new BufferedWriter(new FileWriter(outputFile));
+				
+				// Acts as an holder for a transformation result, which may be XML, plain Text, 
+				// HTML, or some other form of markup.
+				streamResult = new StreamResult(out);
 
-				out.write(output);
-				out.close();
-
-				System.out.println("\nXML Directory Listing successfully generated!");
-
-			} catch (IOException e) {
-				System.out.println("\nERROR: Cannot write to file. Please ensure you have specified a valid file name.");
+				// This class extends TransformerFactory to provide SAX-specific factory methods. It 
+				// provides two types of ContentHandlers, one for creating Transformers, the other 
+				// for creating Templates objects.
+				tf = (SAXTransformerFactory) SAXTransformerFactory.newInstance();
+				
+				// This class provides a default implementation of the SAX2 Attributes interface,
+				// with the addition of manipulators so that the list can be modified or reused.
+				// SAX Helper
+				atts = new AttributesImpl();
+				
+				// A TransformerHandler listens for SAX ContentHandler parse events and 
+				// transforms them to a Result.
+				hd = tf.newTransformerHandler();
+				
+				// An instance of this abstract class can transform a source tree into a
+				// result tree.
+				serializer = hd.getTransformer();
+				
+				// Set an output property that will be in effect for the transformation.
+				serializer.setOutputProperty(OutputKeys.ENCODING,"ISO-8859-1");
+				// serializer.setOutputProperty(OutputKeys.DOCTYPE_SYSTEM,"users.dtd");
+				serializer.setOutputProperty(OutputKeys.INDENT,"yes");
+	
+				// Enables the user of the TransformerHandler to set the to set the Result 
+				// for the transformation.
+				hd.setResult(streamResult);
+				
+				// SAX ContentHandler parse event
+				hd.startDocument();
+				
+				createElement(dir);
+							
+				// SAX ContentHandler parse event
+				hd.endDocument();
+							
+			} catch (Exception e) {
+				
 			}
+			
+		} else {
+			// ERROR DIRECTORY IS NOT VALID
+			System.out.print(dir.getAbsolutePath() + " is not a valid directory.");
+		}
+		
+	}
+	
+	/**
+	 * Gets the absolute path of the file.
+	 * @param file The file to get the path for.
+	 */
+	public static String getAbsolutePath(File file) {
 
-	} else {
+		return file.getAbsolutePath();	
 
-		System.out.println("ERROR: Please ensure that you have specified a directory.");
+	}
+	
+	/**
+	 * When passed a valid file, sets the lastModified attribute.
+	 * @param file The file to get the lastModified for.
+	 */
+	public static String getDate(File file) {
+		
+		// Get the last modified date
+		long lastModified = file.lastModified();
+		
+		// Get as date
+		Date d =  new Date(lastModified);
+		
+		// Convert to string with formatting.
+        String s = dateFormat.format(d);
+        									
+		return s;
+		
+	}
+	
+	/**
+	 * When passed a valid file, sets the fileSize attribute to the size of the file in bytes.
+	 * @param file The file to get the size for.
+	 */
+	public static String getSize(File file) {
+		
+		// Get the number of bytes in the file
+		long length = file.length();
+		
+		// Convert size to string
+		String s = new String();
+		s = String.valueOf(length);
+		
+		return s;
 
 	}
 
-  }
+	/**
+	 * When passed a valid file, sets the lastModified attribute.
+	 * @param file The file to get the lastModified for.
+	 */
+	public static String getLastModified(File file) {
+					
+		// Get the last modified date
+		long lastModified = file.lastModified();
+		
+		// Convert last modified to string
+		String s = new String();
+		s = String.valueOf(lastModified);
+        									
+		return s;
 
-  // Recursively search directory and output details as xml in a string
-  public static void visitAllDirsAndFiles(File dir) {
+	}
+	
+	/**
+	 * Sets the name attribute.
+	 * @param file The file to get the name for.
+	 */
+	public static String getName(File file) {
+		
+		return file.getName();	
+	}	
+	
+	/**
+	 * Gets the type of the File object, returns directort or file.
+	 * @param file The file to get the type for.
+	 */
+	public static String getType(File file) {
+		
+		if (file.isDirectory()) {
+			return "directory";
+		} else {
+			return "file";
+		}
+			
+	}	
+	
+	/**
+	 * Sets all attributes for the file 
+	 * @param file The file to set attributes for
+	 */
+	public static void setAttributes(File file) {
 
+		// Clear current attributes
+		atts.clear();
+		
+		atts.addAttribute("","","name","CDATA",getName(file));
+		atts.addAttribute("","","size","CDATA",getSize(file));
+		atts.addAttribute("","","lastModified","CDATA",getLastModified(file));
+		atts.addAttribute("","","date","CDATA",getDate(file));
+		atts.addAttribute("","","absolutePath","CDATA",getAbsolutePath(file));
+		
+	}
+	
+	/**
+	 * Sets the date format for the class, must be valid SimpleDateFormat
+	 * @param format The Java SimpleDateFormat string.
+	 */
+	public static void setDateFormat(String format) {
+		
+		// Set date format
+		try {
+			dateFormat = new SimpleDateFormat(format);
+		} catch (Exception e) {
+			
+		}
+		
+	}
+
+	/**
+	 * Creates a new element based on the file based through.
+	 * @param file The file on which the element is based.
+	 */
+	public static void createElement(File file) {
+		
+		setAttributes(file);
+		
+		String fileType = getType(file);
+		
+		try{
+			// Output details of the file
+			hd.startElement("","",fileType,atts);
+			
+			if (fileType == "directory") {
+				visitAllDirsAndFiles(file);
+			}
+			
+			hd.endElement("","",fileType);
+		
+		} catch (Exception e) {
+		
+		}
+	}
+	
+	// Recursively search directory and output details as xml in a string
+	public static void visitAllDirsAndFiles(File dir) {
+	
 		// Store all files and folder names in an array and sort alphabetically.
 		String[] children = dir.list();
 		Arrays.sort(children);
-
-		// Loop through all files and folders
-		for (int i=0; i<children.length; i++) {
+		
+		for (int i=0;i<children.length;i++) {
 
 			// Get file or directory
-			File tempFile = new File(dir, children[i]);
+			File file = new File(dir, children[i]);
+					
+			createElement(file);
 
-			// Get the number of bytes in the file
-			long length = tempFile.length();
-
-			// Get the last modified date
-			long lastModified = tempFile.lastModified();
-
-			// Get absolute path
-			String absolutePath = tempFile.getAbsolutePath();
-
-			// Get path
-			String path = tempFile.getPath();
-
-			// Output information on command line if verbose is true
-			if (verbose == true) {
-				System.out.println("ANALYSING:  " + absolutePath);
-			}
-
-			// If the file is a directory output it's details and call this function again to recurse through files
-			if (tempFile.isDirectory()) {
-
-				output += "<directory>";
-				output += "<name>" + children[i] + "</name>";
-				output += "<absolutePath>" + absolutePath + "</absolutePath>";
-				output += "<path>" + path + "</path>";
-
-				visitAllDirsAndFiles(new File(dir, children[i]));
-
-				output += "</directory>";
-
-			} else {
-				// Output details of the file
-				output += "<file>";
-				output += "<name>" + children[i] + "</name>";
-				output += "<absolutePath>" + absolutePath + "</absolutePath>";
-				output += "<path>" + path + "</path>";
-				output += "<fileSize>" + length + "</fileSize>";
-				output += "<lastModified>" + new Date(lastModified) + "</lastModified>";
-				output += "</file>";
-			}
 		}
-
-		// Garbage Collection
+		
 	}
-
-	// Print help information
-	public static void generateHelpInformation() {
-
-		System.out.println("\n Usage: java FileListing <options> <directory> <output file>");
-		System.out.println("\n Where possible options include:");
-		System.out.println("     -v, -verbose:       Print a synopsis of standard options");
-		System.out.println("     -h, -help, -?:      Output messages about what the program is doing \n");
-
-	}
-
+	
 }
