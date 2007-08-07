ba2cf1bfbf5dc6829ad60cf9118dcbe3a9d74623
monsieur.haynes <monsieur.haynes@f29ea386-3431-0410-afd1-c10b351bfbf8>
2007-08-07 17:13:46 +0000

    Finished for day

--- branches/dev_1.0/src/net/matthaynes/xml/dirlist/DirectoryListing.java
+++ branches/dev_1.0/src/net/matthaynes/xml/dirlist/DirectoryListing.java
@@ -4,22 +4,24 @@ import java.io.*;
 import java.util.*;
 
 public final class DirectoryListing {
-	
+
 	// Main function
 	public static void main(String aArguments[]) throws FileNotFoundException, IOException {
-	
+
 		// Get specified directory
-		File dir = new File(aArguments[1]); 
-		
+		File dir = new File(aArguments[0]);
+
+
 		// Set output stream for generated file
 		FileOutputStream out = new FileOutputStream("test.xml");
-		
+
 		// Begin listing
 		XmlDirectoryListing lister = new XmlDirectoryListing();
 		lister.generateXmlDirectoryListing(dir, out);
-		
+
 		// Close output stream
 		out.close();
+
 	}
-	
+
 }
