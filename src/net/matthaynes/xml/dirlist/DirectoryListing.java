7272ff6938da1bd425f6bd464ecf2e839475833b
monsieur.haynes <monsieur.haynes@f29ea386-3431-0410-afd1-c10b351bfbf8>
2007-08-13 17:51:39 +0000

    Finished 4 day

--- src/net/matthaynes/xml/dirlist/DirectoryListing.java
+++ src/net/matthaynes/xml/dirlist/DirectoryListing.java
@@ -3,12 +3,45 @@ package net.matthaynes.xml.dirlist;
 import java.io.*;
 import java.text.SimpleDateFormat;
 import java.util.*;
+import org.apache.commons.cli.*;
 
 public final class DirectoryListing {
-
+	
+	// Store options
+	Options opt = new Options();
+	
+	// Help formatter
+	HelpFormatter f = new HelpFormatter();
+	
 	// Main function
-	public static void main(String aArguments[]) throws FileNotFoundException, IOException {
+	public static void main(String aArguments[]) throws FileNotFoundException, IOException, ParseException {
 
+		
+		try {
+		      
+
+		      opt.addOption("h", false, "Print help for this application");
+		      opt.addOption("u", true, "The username to use");
+		      opt.addOption("dsn", true, "The data source to use");
+
+		      BasicParser parser = new BasicParser();
+		      CommandLine cl = parser.parse(opt, args);
+
+		      if ( cl.hasOption(´h´) ) {
+		    	  this.displayHelp();
+		      }       else {
+		        System.out.println(cl.getOptionValue("u"));
+		        System.out.println(cl.getOptionValue("dsn"));
+		      }
+		    }
+		    catch (ParseException e) {
+		      e.printStackTrace();
+		    }
+		  }
+		
+
+		// ++++++++++++++++++++++++++++++++++++++++++++++++++++++
+		
 		// Get specified directory
 		File dir = new File(aArguments[0]);
 
@@ -25,5 +58,10 @@ public final class DirectoryListing {
 		out.close();
 
 	}
+
+	public void displayHelp() {
+		this.f.printHelp("OptionsTip", opt);
+	}
+	
 
 }
