e6b2ed0c6795154b9afbf2918af112acc0bfa0ca
monsieur.haynes <monsieur.haynes@f29ea386-3431-0410-afd1-c10b351bfbf8>
2009-10-08 17:31:33 +0000

    Possible fix to spaces in file name bug

--- bin/xml-dir-listing.bat
+++ bin/xml-dir-listing.bat
@@ -11,10 +11,10 @@ set XML_DIR_LISTING_HOME=%~dp0..
 
 REM Get local classpath
 
-set LOCAL_CP=%CLASSPATH%;%XML_DIR_LISTING_HOME%\lib\xml-dir-listing.0.1.jar;%XML_DIR_LISTING_HOME%\lib\commons-cli-1.1.jar;%XML_DIR_LISTING_HOME%\lib\jakarta-regexp-1.5.jar;%XML_DIR_LISTING_HOME%\lib\log4j-1.2.14.jar
+set LOCAL_CP=%CLASSPATH%;%XML_DIR_LISTING_HOME%\lib\xml-dir-listing.0.2.jar;%XML_DIR_LISTING_HOME%\lib\commons-cli-1.1.jar;%XML_DIR_LISTING_HOME%\lib\jakarta-regexp-1.5.jar;%XML_DIR_LISTING_HOME%\lib\log4j-1.2.14.jar
 
 REM echo %LOCAL_CP%
 
 REM Run app
 
-java -classpath "%LOCAL_CP%" net.matthaynes.xml.dirlist.DirectoryListing %*
+java -classpath "%LOCAL_CP%" net.matthaynes.xml.dirlist.DirectoryListing "%*"
